f246c93ceeb5da8fec42fee0c462be9f5048f61c
胡其成 <huqicheng@huqichengdeMacBook-Air.local>
2017-10-25 15:06:04 -0400

    update UI listener

--- app/src/main/java/com/example/huqicheng/service/MyService.java
+++ app/src/main/java/com/example/huqicheng/service/MyService.java
@@ -2,6 +2,7 @@ package com.example.huqicheng.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -13,13 +14,36 @@ import com.google.gson.Gson;
 
 public class MyService extends Service {
     private static NettyClientBootstrap client = null;
+
+    //Listener only for WeChatActivity
+    public static OnChatMsgRecievedListener onChatMsgRecievedListener;
+
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new MsgBinder();
+
+    }
+
+    public class MsgBinder extends Binder {
+        public MyService getService(){
+            return MyService.this;
+        }
+    }
+
+    //set listener for UI updating
+    public void setOnChatMsgRecievedListener(OnChatMsgRecievedListener listener){
+        onChatMsgRecievedListener = listener;
+    }
+
+    //static method for handler
+    public static void onChatMsgRecieved(BaseMsg msg){
+        if(onChatMsgRecievedListener == null){
+            return;
+        }
+        onChatMsgRecievedListener.onChatMsgRecieved(msg);
     }
 
     @Override
@@ -45,17 +69,17 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-//        Log.d("debug:","service");
-//        client = ClientUtils.getInstance();
-//
-//        Log.d("debug:","start");
-//
-//        BaseMsg loginMsg=new BaseMsg();
-//        loginMsg.setType(MsgType.LOGIN);
-//        loginMsg.putParams("user", "huqicheng");
-//        loginMsg.putParams("pwd", "huqicheng");
-//        if(client.socketChannel != null){
-//            client.socketChannel.writeAndFlush(new Gson().toJson(loginMsg));
-//        }
+        Log.d("debug:","service");
+        client = ClientUtils.getInstance();
+
+        Log.d("debug:","start");
+
+        BaseMsg loginMsg=new BaseMsg();
+        loginMsg.setType(MsgType.LOGIN);
+        loginMsg.putParams("user", "huqicheng");
+        loginMsg.putParams("pwd", "huqicheng");
+        if(client.socketChannel != null){
+            client.socketChannel.writeAndFlush(new Gson().toJson(loginMsg));
+        }
     }
 }
